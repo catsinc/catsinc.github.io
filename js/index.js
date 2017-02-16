6c241edbec2b8fbee097e2b82e5803e31c96ea9f
loi.wu <superloiwu@@yahoo.com>
2017-02-17 00:13:40 +0800

    Cat Inc. - Tiger and Shiny are running fast

---
+++ js/index.js
@@ -0,0 +1,9 @@
+var cat1 = document.getElementById('cat1');
+cat1.addEventListener('click', () => {
+  cat1.style.animationPlayState = 'running'
+});
+
+var cat2 = document.getElementById('cat2');
+cat2.addEventListener('click', () => {
+  cat2.style.animationPlayState = 'running'
+});
